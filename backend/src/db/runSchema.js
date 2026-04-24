aa01e2815af929548ce124470c575891a03663e6
Villy Siu <villysiu@gmail.com>
2026-04-24 06:38:50 -0700

    Fix typos and correct file names

--- backend/src/db/runSchema.js
+++ backend/src/db/runSchema.js
@@ -4,7 +4,6 @@ const path = require("path");
 const pool = require("../config/db.postgres");
 
 async function runSchema() {
-  console.log("DB:", process.env.DATABASE_URL);
     try {
     const sql = fs.readFileSync(
       path.join(__dirname, "schema.sql"),
