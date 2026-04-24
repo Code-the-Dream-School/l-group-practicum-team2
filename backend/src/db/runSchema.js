ff4dd3987ecb009daab67d2f3108a0bc505bb612
Villy Siu <villysiu@gmail.com>
2026-04-24 06:38:50 -0700

    Fix typos and correct file names

--- backend/src/db/runSchema.js
+++ backend/src/db/runSchema.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const pool = require("./index");
 
-async fucntion runSchema() {
+async function runSchema() {
     try {
     const sql = fs.readFileSync(
       path.join(__dirname, "schema.sql"),
