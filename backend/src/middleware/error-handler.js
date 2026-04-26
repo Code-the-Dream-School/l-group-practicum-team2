38928f3acc010b3fd53683ca8775e1ad8be9a5a5
Villy Siu <villysiu@gmail.com>
2026-04-25 20:45:25 -0700

    Add PostgreSQL error code handling (23505, 23503, 22P02, 23502) and unify API error response format

---
+++ backend/src/middleware/error-handler.js
@@ -0,0 +1,42 @@
+const { StatusCodes } = require('http-status-codes')
+const errorHandlerMiddleware = (err, req, res, next) => {
+  
+    console.log(err)
+    let customError = {
+        // set default
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong try again later',
+    }
+
+    // PostgreSQL: unique violation 23505
+    if (err.code === '23505') {
+        customError.msg = `Duplicate value entered, please choose another value`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    // PostgreSQL: Foreign key violation 23503
+    if (err.code === '23503') {
+        customError.msg = `Related record not found or violates constraint`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+    // PostgreSQL: Invalid input syntax (bad ID/type) 22P02
+    if (err.code === '22P02') {
+        customError.msg = `Invalid input syntax provided`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    // PostgreSQL: not_null_violation 23502
+    if (err.code === '23502') {
+        customError.msg = `Missing required field: ${err.column || 'unknown'}`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+
+
+    return res.status(customError.statusCode).json({ 
+        success: false,
+        message: customError.msg 
+    })
+}
+
+module.exports = errorHandlerMiddleware
