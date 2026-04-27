4d29428a16eb01fe49dc16748bac29acc43aeee8
Villy Siu <villysiu@gmail.com>
2026-04-27 16:31:56 -0700

    add database connection test on server startup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const app = require('./src/app');
+const pool = require('./src/config/db.postgres')
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    // test DB connection
+    await pool.query('SELECT 1');
+
+    console.log('Database connected successfully');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    })
+  } catch (error) {
+    console.error('Database connection failed');
+    console.error(error.message);
+    console.error(error)
+    process.exit(1); // stop app if DB fails
+  }
+}
+
+start();
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
