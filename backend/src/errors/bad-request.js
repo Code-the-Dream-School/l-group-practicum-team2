89ee2450d09f2d662157c2b6cf6f607269e99fc8
Villy Siu <villysiu@gmail.com>
2026-04-27 16:25:28 -0700

    Add default messages to custom API error classes

--- backend/src/errors/bad-request.js
+++ backend/src/errors/bad-request.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
 class BadRequestError extends CustomAPIError {
-  constructor(message) {
+  constructor(message = '400 Bad Request') {
     super(message);
     this.statusCode = StatusCodes.BAD_REQUEST;
   }
--- backend/src/errors/unauthenticated.js
+++ backend/src/errors/unauthenticated.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
 class UnauthenticatedError extends CustomAPIError {
-  constructor(message) {
+  constructor(message = '401 Authentication invalid') {
     super(message);
     this.statusCode = StatusCodes.UNAUTHORIZED;
   }
