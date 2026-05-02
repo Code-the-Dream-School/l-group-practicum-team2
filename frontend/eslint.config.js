d8f57c619210a096006109a582498501e76cd1dd
Villy Siu <villysiu@gmail.com>
2026-05-02 11:04:01 -0700

    remove TypeScript config and migrate project to JS/JSX

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,10 +8,9 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
   globalIgnores(["dist"]),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
