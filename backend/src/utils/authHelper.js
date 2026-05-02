3cbf1e34a03324c33b99a9e7521e33cea2a81416
Villy Siu <villysiu@gmail.com>
2026-05-01 17:13:16 -0700

    add missing export for auth helper functions

--- backend/src/utils/authHelper.js
+++ backend/src/utils/authHelper.js
@@ -4,6 +4,9 @@ async function hashPassword(password) {
   return await bcrypt.hash(password, 10);
 }
 
-async function comparePassword(password, hashedPassword) {
-  return await bcrypt.compare(password, hashedPassword);
-}
+module.exports = { hashPassword };
+// async function comparePassword(password, hashedPassword) {
+//   return await bcrypt.compare(password, hashedPassword);
+// }
+
+// module.exports = { hashPassword, comparePassword };
