cc4087defadd2a46ba69025700da55b6ac761f65
Heather813903 <heathersmith9378@gmail.com>
2026-05-02 23:49:49 -0400

    chore: format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
-
- 
 function App() {
   return (
     <Routes>
