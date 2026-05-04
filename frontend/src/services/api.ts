5a95b32574821793487a8b0009f8849b9312bc2a
Heather813903 <heathersmith9378@gmail.com>
2026-05-04 13:47:30 -0400

    chore: format api service

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,13 +1,13 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function getHelloMessage() {
-    const response = await fetch(`${API_BASE_URL}/api/hello`);
+  const response = await fetch(`${API_BASE_URL}/api/hello`);
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if(!response.ok) {
-        throw new Error(data.error || "Failed to fetch from backend")
-    }
+  if (!response.ok) {
+    throw new Error(data.error || "Failed to fetch from backend");
+  }
 
-    return data;
+  return data;
 }
