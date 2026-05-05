0c33ed3dd547f897a3b9599bf905b536abbb0c40
Aya D <dzhaicholpon@gmail.com>
2026-05-05 12:09:58 -0500

    build animal detail page UI with routing and 404 handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import AnimalList from "./pages/animals/AnimalList";
+import AnimalDetail from "./pages/animals/AnimalDetail";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 function App() {
-  return <AnimalList />;
+  return (
+    <Routes>
+      <Route path="/" element={<AnimalList />} />
+      <Route path="/animals/:id" element={<AnimalDetail />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
+
 export default App;
