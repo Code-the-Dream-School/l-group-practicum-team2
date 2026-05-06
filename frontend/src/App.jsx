951aa0a7eef35243bbd382e3143f71f4f36f1b81
volha83 <o.podlipskaya@gmail.com>
2026-05-05 23:45:58 -0400

    Add favorites route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AnimalList from "./pages/animals/AnimalList";
 import "./App.css";
+import FavoritesPage from "./pages/favorites/favoritesPage";
 
 function App() {
-  return <AnimalList />;
+  return (
+    <BrowserRouter>
+      <nav className="app-nav">
+        <h2>PawMatch</h2>
+        <Link to="/favorites" className="favorites-link">Favorites</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<AnimalList />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  
 }
 export default App;
---
+++ frontend/src/pages/favorites/FavoritesPage.jsx
@@ -0,0 +1,9 @@
+function FavoritesPage() {
+    return (
+    <main className="app">
+        <h1>Favorites</h1>
+    </main>
+    );
+}
+
+export default FavoritesPage;
