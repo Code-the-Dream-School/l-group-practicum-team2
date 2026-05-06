e7d35fce863e024bafa48973a93a4dd4873259a8
Heather813903 <heathersmith9378@gmail.com>
2026-05-06 11:18:46 -0400

    fix: resolve merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AnimalList />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
