643e691b7dcc26c139c1c2bc4204a24f4f4e4d06
Villy Siu <villysiu@gmail.com>
2026-05-07 06:08:01 -0700

    convert userData typing from TypeScript to JavaScript

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,22 +1,12 @@
+
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
 
-type RegisterData = {
-  name: string;
-  email: string;
-  password: string;
-};
-
-type LoginData = {
-  email: string;
-  password: string;
-};
-
-export async function registerUser(userData: RegisterData) {
+export async function registerUser(userData) {
   const response = await fetch(`${API_BASE_URL}/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
+      "Accept": "application/json",
     },
     body: JSON.stringify(userData),
   });
@@ -30,12 +20,12 @@ export async function registerUser(userData: RegisterData) {
   return data;
 }
 
-export async function loginUser(userData: LoginData) {
+export async function loginUser(LoginData) {
   const response = await fetch(`${API_BASE_URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
+      "Accept": "application/json",
     },
     body: JSON.stringify(userData),
   });
