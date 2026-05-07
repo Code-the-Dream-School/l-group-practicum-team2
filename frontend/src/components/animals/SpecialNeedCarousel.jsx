58e94fa5c1b2a7164bfe37b261638cf4ee6102c6
Villy Siu <villysiu@gmail.com>
2026-05-07 14:50:45 -0700

    format code with prettier

--- frontend/src/components/animals/SpecialNeedCarousel.jsx
+++ frontend/src/components/animals/SpecialNeedCarousel.jsx
@@ -1,13 +1,13 @@
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import Toast from "react-bootstrap/Toast";
-import {Row, Col} from "react-bootstrap"
+import { Row, Col } from "react-bootstrap";
 import { useState } from "react";
 
 import { useSpecialNeeds } from "../../services/SpecialNeedContext";
 import { useNavigate } from "react-router-dom";
 import AnimalCard from "./AnimalCard";
-import { HeartFill } from "react-bootstrap-icons"
+import { HeartFill } from "react-bootstrap-icons";
 const SpecialNeedCarousel = () => {
   const { specialNeeds } = useSpecialNeeds();
   const [show, setShow] = useState(false);
@@ -21,34 +21,36 @@ const SpecialNeedCarousel = () => {
   };
   return (
     <>
-    <Row>
-      <Col xs="auto" style={{ padding: '2rem 1rem'}}>
-        <div
+      <Row>
+        <Col xs="auto" style={{ padding: "2rem 1rem" }}>
+          <div
+            style={{
+              width: "5rem",
+              height: "5rem",
+              borderRadius: "50%",
+              backgroundColor: "orange",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <HeartFill size={40} color="white" />
+          </div>
+        </Col>
+        <Col
+          className="flex-grow-1"
           style={{
-            width: '5rem',
-            height: '5rem',
-            borderRadius: '50%',
-            backgroundColor: 'orange',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            alignItems: "flexStart",
+            flexDirection: "column",
+            justifyContent: "center",
           }}
         >
-          <HeartFill size={40} color="white" />
-          </div>
-      </Col>
-      <Col className="flex-grow-1" 
-          style={{
-                  'display': 'flex',
-                  'alignItems': 'flexStart',
-                  'flexDirection': 'column',
-                  'justifyContent': 'center'
-                }}>
-        <h2>Need Extra Love</h2>
-        Senior and special needs companions waiting for a home
-      </Col>
-    </Row>
-      
+          <h2>Need Extra Love</h2>
+          Senior and special needs companions waiting for a home
+        </Col>
+      </Row>
+
       <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
         <Toast.Body>Coming soon</Toast.Body>
       </Toast>
@@ -82,14 +84,12 @@ const SpecialNeedCarousel = () => {
                       cursor: "pointer",
                       position: "relative",
                       display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    width: '540px',
-                    
+                      flexDirection: "column",
+                      alignItems: "center",
+                      width: "540px",
                     }}
                   >
                     <AnimalCard animal={specialNeed} />
-                    
                   </div>
                 </div>
               </Carousel.Item>
