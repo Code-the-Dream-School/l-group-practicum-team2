20ea67d493be122105fcdeb66e0e6bb58686ceb4
Villy Siu <villysiu@gmail.com>
2026-05-09 17:21:07 -0700

    fix code formatting with Prettier

--- frontend/src/components/navbar/DropdownLogout.jsx
+++ frontend/src/components/navbar/DropdownLogout.jsx
@@ -1,11 +1,7 @@
 import { Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const DropdownLogout = ({onLogout}) => {
-  return (
-    <Dropdown.Item  onClick={onLogout}>
-      Logout
-    </Dropdown.Item>
-  );
+const DropdownLogout = ({ onLogout }) => {
+  return <Dropdown.Item onClick={onLogout}>Logout</Dropdown.Item>;
 };
 export default DropdownLogout;
