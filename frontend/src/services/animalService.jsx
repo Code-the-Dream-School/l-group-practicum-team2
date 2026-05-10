1cf5e0906d0fa2b74c65127040c3b0c6a35bde80
Villy Siu <villysiu@gmail.com>
2026-05-09 18:59:18 -0700

    implement AnimalContext for global animal state management

---
+++ frontend/src/services/animalService.jsx
@@ -0,0 +1,52 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const AnimalContext = createContext();
+
+export const AnimalProvider = ({ children }) => {
+  const BACKEND_API = import.meta.env.VITE_BACKEND_API;
+
+  const [loading, setLoading] = useState(false);
+  const [animals, setAnimals] = useState([]);
+  const [error, setError] = useState(null);
+
+  const getAnimals = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `${BACKEND_API}/api/animals?status=available`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.msg || "Failed to fetch animals");
+      }
+      setAnimals(data.animals || []);
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAnimals();
+  }, []);
+
+  return (
+    <AnimalContext.Provider
+      value={{ animals, loading, error }}
+    >
+      {children}
+    </AnimalContext.Provider>
+  );
+};
+
+export const useAnimal = () => useContext(AnimalContext);
