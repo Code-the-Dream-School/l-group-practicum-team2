f283fa5f8abc20a92ba6db7cd950d6691bfeed42
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2026-05-11 14:12:17 -0400

    Created middleware to validate data in Post Favorite Animal, Login User, and Register User endpoints

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, getCurrentUser } = require('../controllers/auth.controller');
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require('../controllers/auth.controller');
 const authenticateUser = require('../middleware/authentication');
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../middleware/input-validation');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegisterInput, register);
+router.post('/login', validateLoginInput, login);
 router.get('/me', authenticateUser, getCurrentUser);
 
 module.exports = router;
