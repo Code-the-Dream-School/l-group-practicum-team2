cf3ba2374d862a6ae96441db8e000368a54deb12
Villy Siu <villysiu@gmail.com>
2026-05-11 14:48:54 -0700

    Refactor fetchCurrentUser into service layer

--- frontend/src/services/authService.jsx
+++ frontend/src/services/authService.jsx
@@ -35,3 +35,36 @@ export async function loginUser(userData) {
 
   return data;
 }
+
+export const fetchCurrentUser = async () => {
+    
+    if (!localStorage.getItem("token")) {
+      setLoading(false);
+      return null;
+    }
+
+    // await new Promise((resolve)=>setTimeout(resolve, 2000))
+    try {
+      const response = await fetch(`${API_BASE_URL}/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error || "Login failed");
+      }
+      
+      return  {
+                'success': true, 
+                'user': data
+              };
+    } catch (error) {
+      return  {
+                'success': false,
+                'error': error.message}
+              }
+  };
