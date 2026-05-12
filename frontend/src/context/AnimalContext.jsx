af959a2541e7027b715530cf9ce5cb495bae8478
Villy Siu <villysiu@gmail.com>
2026-05-11 19:06:43 -0700

    add fetchAnimals service function

---
+++ frontend/src/context/AnimalContext.jsx
@@ -0,0 +1,26 @@
+const BACKEND_API = import.meta.env.VITE_BACKEND_API;
+
+export const fetchAnimals = async () => {
+    
+    try{
+        const response = await fetch(
+            `${BACKEND_API}/api/animals?status=available`,
+            {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                },
+            }
+        )
+        
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || "Failed to fetch animals");
+        }
+        return data
+
+    } catch (error) {
+        throw new Error(error.message || "Network error");
+    }
+}
