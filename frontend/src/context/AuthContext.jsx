3e51d137c1607fcfbc94ed772383da30c35460d1
Villy Siu <villysiu@gmail.com>
2026-05-12 19:07:48 -0700

    format code using Prettier

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,13 +4,12 @@ import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    
   const [user, setUser] = useState(
-     // for testing only — remove when backend auth is ready
+    // for testing only — remove when backend auth is ready
     {
-        id: "e070302d-2b83-44b4-b728-aa0b597fdfeb",
-        name: "John",
-        email: "user1@bb.com",
+      id: "e070302d-2b83-44b4-b728-aa0b597fdfeb",
+      name: "John",
+      email: "user1@bb.com",
     }
 
     // production version:
@@ -22,30 +21,25 @@ export const AuthProvider = ({ children }) => {
 
   const navigate = useNavigate();
 
-  
-
   const logoutUser = async (withLoading = true) => {
-    if (withLoading) 
-        setLoading(true);
+    if (withLoading) setLoading(true);
 
     try {
-        setUser(null);
-        // uncomment when FavoriteContext and InquiryCOntext is ready
-        // setFavorites([]); 
-        // setInquiries([]);     
-        localStorage.removeItem("token");
-
+      setUser(null);
+      // uncomment when FavoriteContext and InquiryCOntext is ready
+      // setFavorites([]);
+      // setInquiries([]);
+      localStorage.removeItem("token");
     } catch (error) {
-        console.error(error);
+      console.error(error);
     } finally {
-        if (withLoading) setLoading(false);
+      if (withLoading) setLoading(false);
     }
   };
 
-  useEffect(()=>{
-    console.log("user changed:" ,user)
-  }, [user])
-
+  useEffect(() => {
+    console.log("user changed:", user);
+  }, [user]);
 
   return (
     <AuthContext.Provider
@@ -53,7 +47,7 @@ export const AuthProvider = ({ children }) => {
         user,
         error,
         loading,
-        logoutUser
+        logoutUser,
       }}
     >
       {children}
