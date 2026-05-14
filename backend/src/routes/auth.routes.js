b8aceae773f3442ee8a38e1fb3edd66a0b1aa7a0
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2026-05-14 09:04:04 -0400

    Added and configured validateInquiryInput validator. Also moved validators to src/validators folder.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -12,7 +12,7 @@ const authenticateUser = require('../middleware/authentication');
 const {
   validateRegisterInput,
   validateLoginInput,
-} = require('../middleware/input-validation');
+} = require('../validators/input-validation');
 
 router.post('/register', validateRegisterInput, register);
 router.post('/login', validateLoginInput, login);
--- backend/src/validators/input-validation.js
+++ backend/src/validators/input-validation.js
@@ -1,6 +1,8 @@
 const Joi = require('joi');
 const BadRequestError = require('../errors/bad-request');
 const passwordRegex = '^[a-zA-Z0-9]{3,30}$';
+const uuidRegex =
+  '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$';
 
 const validateRegisterInput = (req, res, next) => {
   const { name, email, password } = req.body;
@@ -32,9 +34,6 @@ const validateLoginInput = (req, res, next) => {
 const validateFavoriteInput = (req, res, next) => {
   const user_id = req.user.id;
   const { animal_id } = req.body;
-  const uuidRegex =
-    '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$';
-  //   check if animal id and user id exists in the database
   const schema = Joi.object({
     user_id: Joi.string().pattern(new RegExp(uuidRegex)).required(),
     animal_id: Joi.string().pattern(new RegExp(uuidRegex)).required(),
@@ -46,8 +45,24 @@ const validateFavoriteInput = (req, res, next) => {
   next();
 };
 
+const validateInquiryInput = (req, res, next) => {
+  const { animal_id, message } = req.body;
+  const user_id = req.user.id;
+  const schema = Joi.object({
+    user_id: Joi.string().pattern(new RegExp(uuidRegex)).required(),
+    animal_id: Joi.string().pattern(new RegExp(uuidRegex)).required(),
+    message: Joi.string().min(10).required(),
+  });
+  const valid = schema.validate({ user_id, animal_id, message });
+  if (valid.error) {
+    throw new BadRequestError(valid.error);
+  }
+  next();
+};
+
 module.exports = {
   validateRegisterInput,
   validateLoginInput,
   validateFavoriteInput,
+  validateInquiryInput,
 };
