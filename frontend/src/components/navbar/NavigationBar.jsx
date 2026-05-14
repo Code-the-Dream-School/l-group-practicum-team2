7515d69486ad9f82b338dfb83898e7da9ab427c1
Aya D <dzhaicholpon@gmail.com>
2026-05-13 20:41:23 -0500

    fix: restore Link import in NavigationBar

--- frontend/src/components/navbar/NavigationBar.jsx
+++ frontend/src/components/navbar/NavigationBar.jsx
@@ -1,6 +1,4 @@
 import { Button, Container, Image, Nav, Navbar } from "react-bootstrap";
-import { Navbar, Nav, Container, Button, Image } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { HeartFill, PersonFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import logo from "../../assets/PawMatch.png";
