00d9ae14b87ae51f1a13ef658a9e3c201d218585
Villy Siu <villysiu@gmail.com>
2026-05-16 21:57:50 -0700

    refactor Heart component

--- frontend/src/components/animals/Heart.jsx
+++ frontend/src/components/animals/Heart.jsx
@@ -1,29 +1,44 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import { addFavorite, removeFavorite } from "../../services/favorites";
+import { useFavorite } from "../../context/favoriteContext";
+import { useAuth } from "../../context/authContext";
 
-function Heart({ animalId, initialFavorite = false }) {
-  const [favorite, setFavorite] = useState(initialFavorite);
-  const navigate = useNavigate();
+// user's  favorites
+// [ 
+// 	{
+// 		"id": "bd74582f-262c-4429-a975-271b91b01635",
+// 	},
+// 	{
+// 		"id": "c8d91d82-232f-4383-bf62-cbbc5a69d1c2",
+// 	},
+// 	{
+// 		"id": "6142e474-d91f-4940-b84b-d7a0e3e2a6df",
+// 	},
+// 	{
+// 		"id": "4bc45f9c-e0c7-40c9-a3b3-5fbe64fb8bf3",
+// 	}
+// ]
+function Heart({ animalId }) {
+  const { addFavorite, removeFavorite, isFavorite } = useFavorite();
+  const { currentUser } = useAuth();
+const navigate = useNavigate();
+  const favorite = isFavorite(animalId);
+
 
   function handleClick(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    const token = localStorage.getItem("token");
-
-    if (!token) {
+    if (!currentUser) {
       navigate("/login");
       return;
     }
 
     if (favorite) {
       removeFavorite(animalId);
-      setFavorite(false);
     } else {
       addFavorite(animalId);
-      setFavorite(true);
     }
   }
 
@@ -42,7 +57,7 @@ function Heart({ animalId, initialFavorite = false }) {
 Heart.propTypes = {
   animalId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     .isRequired,
-  initialFavorite: PropTypes.bool,
+  // initialFavorite: PropTypes.bool,
 };
 
 export default Heart;
