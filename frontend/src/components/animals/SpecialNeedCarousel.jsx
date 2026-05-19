f227a2153a7021f2b46bee58fd72d9c4a26f6c2f
Villy Siu <villysiu@gmail.com>
2026-05-18 20:52:03 -0700

    import error and add loading state from useAnimal hook

--- frontend/src/components/animals/SpecialNeedCarousel.jsx
+++ frontend/src/components/animals/SpecialNeedCarousel.jsx
@@ -9,10 +9,9 @@ import LoadingSpinner from "../LoadingSpinner";
 import { useAnimal } from "../../contexts/AnimalContext";
 
 const SpecialNeedCarousel = () => {
-<<<<<<< HEAD
-  const { animals } = useAnimal();
+  const { animals, loading, error } = useAnimal();
 
-  const specialNeedsAnimals = animals.filter(a => a.special_needs);
+  const specialNeedsAnimals = animals.filter((a) => a.special_needs);
   const [show, setShow] = useState(false);
 
   // uncomment when detail page integrated with AnimalContext is merged
@@ -22,62 +21,51 @@ const SpecialNeedCarousel = () => {
     // uncomment when detail page integrated with AnimalContext is merged
     // navigate(`/animals/${id}`);
   };
-  if(loading)
+  if (loading)
     return <LoadingSpinner message="Loading special needs companions..." />;
-  
-=======
-  const { specialNeedsAnimals } = useAnimal();
 
->>>>>>> cb37b69a (run prettier format)
   return (
     <>
-          <Row>
-            <Col xs="auto" style={{ padding: "2rem 1rem" }}>
-              <div
-                style={{
-                  width: "5rem",
-                  height: "5rem",
-                  borderRadius: "50%",
-                  backgroundColor: "orange",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <HeartFill size={40} color="white" />
-              </div>
-            </Col>
-            <Col
-              className="flex-grow-1"
-              style={{
-                display: "flex",
-                alignItems: "flexStart",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-            >
-              <h2>Need Extra Love</h2>
-              Senior and special needs companions waiting for a home
-            </Col>
-          </Row>
-
-<<<<<<< HEAD
-          <Toast
-            onClose={() => setShow(false)}
-            show={show}
-            delay={2000}
-            autohide
+      <Row>
+        <Col xs="auto" style={{ padding: "2rem 1rem" }}>
+          <div
+            style={{
+              width: "5rem",
+              height: "5rem",
+              borderRadius: "50%",
+              backgroundColor: "orange",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <Toast.Body>Coming soon</Toast.Body>
-          </Toast>
-        
-      {/* // <ErrorMessage
-      //   message="Failed to load special needs companions. Please try again."
-      //   handleRetry={getSpecialNeed}
-      //   error={error}
-      // /> */}
-=======
->>>>>>> 9136e873 (remove unnecessary logic to keep component UI pure)
+            <HeartFill size={40} color="white" />
+          </div>
+        </Col>
+        <Col
+          className="flex-grow-1"
+          style={{
+            display: "flex",
+            alignItems: "flexStart",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h2>Need Extra Love</h2>
+          Senior and special needs companions waiting for a home
+        </Col>
+      </Row>
+
+      <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
+        <Toast.Body>Coming soon</Toast.Body>
+      </Toast>
+
+      {error && (
+        <ErrorMessage
+          message="Failed to load special needs companions. Please try again."
+          error={error}
+        />
+      )}
       <div
         style={{
           width: "992px",
