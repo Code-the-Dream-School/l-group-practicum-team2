640fe897df1c4bc7fd1dba38ef6ec0f16a3025ce
volha83 <o.podlipskaya@gmail.com>
2026-05-21 15:14:00 -0400

    feat: display notification alerts

---
+++ frontend/src/components/notifications/NotificationContainer.jsx
@@ -0,0 +1,31 @@
+import Alert from "react-bootstrap/Alert";
+import { useNotification } from "../../contexts/NotificationContext";
+
+function NotificationContainer() {
+  const { notifications, removeNotification } = useNotification();
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "1rem",
+        right: "1rem",
+        zIndex: 9999,
+        width: "320px",
+      }}
+    >
+      {notifications.map((notification) => (
+        <Alert
+          key={notification.id}
+          variant={notification.type === "error" ? "danger" : notification.type}
+          dismissible
+          onClose={() => removeNotification(notification.id)}
+        >
+          {notification.message}
+        </Alert>
+      ))}
+    </div>
+  );
+}
+
+export default NotificationContainer;
