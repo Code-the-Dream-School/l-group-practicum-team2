609941d89e28cadc2bfe57196853f1deb1e054cd
Villy Siu <villysiu@gmail.com>
2026-05-22 08:06:45 -0700

    add switch case for route-specific loading skeletons

--- frontend/src/components/routes/ProtectedRoute.jsx
+++ frontend/src/components/routes/ProtectedRoute.jsx
@@ -1,12 +1,16 @@
 import React, {useEffect, useRef } from 'react';
 import { useAuth } from "../../contexts/AuthContext";
-import { Navigate } from 'react-router-dom';
-
+import { Navigate, useLocation } from 'react-router-dom';
+import ProfilePlaceholder from '../placeholders/ProfilePlaceholder';
+import InquiryPlaceholder from '../placeholders/InquiryPlaceholder';
+import FavoritePlaceholder from '../placeholders/AnimalListPlaceholder'
 const ProtectedRoute = ({ children }) => {
     const { user, loading: authLoading, openLogin, authModal } = useAuth();
-    
+    const location = useLocation();
   const hasOpenedLogin = useRef(false);
 
+console.log(location)
+  
     useEffect(()=>{
         console.log("authLoading", authLoading, 'user',user, 'authModal', authModal, 'hasOpenedLogin', hasOpenedLogin)
         if(!authLoading && !user && !authModal && !hasOpenedLogin.current){
@@ -19,17 +23,24 @@ const ProtectedRoute = ({ children }) => {
 
     if(!user){
         if (hasOpenedLogin.current && authModal === null) {
-    
             return <Navigate to="/" replace />
         }
-    return <div> skeleton</div>;
+        switch (location.pathname) {
+            case '/profile':
+                return <ProfilePlaceholder />;
+            case '/favorites':
+                return <FavoritePlaceholder />;
+         
+            case '/inquiry':
+                return <InquiryPlaceholder />;
+
+  
+    default:
+        break;
+  }
         
-       
-    }
-
-
     
-      
+    }
 
     return children;
 }
