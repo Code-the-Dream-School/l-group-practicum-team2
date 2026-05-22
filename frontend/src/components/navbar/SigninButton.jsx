d1f4f5ee9404150ecf65d084f03d15355d369fc8
Villy Siu <villysiu@gmail.com>
2026-05-22 16:49:23 -0700

    trigger login modal through AuthContex

--- frontend/src/components/navbar/SigninButton.jsx
+++ frontend/src/components/navbar/SigninButton.jsx
@@ -1,25 +1,15 @@
-import LoginModal from "../auth/LoginModal";
-import SignupModal from "../auth/SignupModal";
-import { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { PersonFill } from "react-bootstrap-icons";
+import { useAuth } from '../../contexts/AuthContext';
 
 export const SigninButton = () => {
-  const [show, setShow] = useState(null);
-
+  const { openLogin } = useAuth();
   return (
     <>
-      <Modal show={show !== null} onHide={() => setShow(null)} size="md">
-        {show === "login" ? (
-          <LoginModal setShow={setShow} />
-        ) : (
-          <SignupModal setShow={setShow} />
-        )}
-      </Modal>
       <Button
         variant="primary"
         className="px-4 py-2"
-        onClick={() => setShow("login")}
+        onClick={() => openLogin()}
       >
         <PersonFill size={25} className="me-1" />
         <span className="fs-5">Sign in</span>
