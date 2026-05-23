251f861af66a793eb43d411c60c4778dfc7c73fc
Villy Siu <villysiu@gmail.com>
2026-05-23 14:58:50 -0700

    run prettier format

--- frontend/src/contexts/InquiryContext.jsx
+++ frontend/src/contexts/InquiryContext.jsx
@@ -5,95 +5,86 @@ import React, {
   useState,
   useCallback,
 } from "react";
-import {
-  getUserInquiries,
-  addInquiry,
-
-} from "../services/inquiryService";
+import { getUserInquiries, addInquiry } from "../services/inquiryService";
 import { useAuth } from "./AuthContext";
 
 const InquiryContext = createContext();
 
 export const InquiryProvider = ({ children }) => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [inquiries, setInquiries] = useState([]);
-    const [pendingMessageObj, setPendingMessageObj] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [inquiries, setInquiries] = useState([]);
+  const [pendingMessageObj, setPendingMessageObj] = useState(null);
 
-    const { user, openLogin } = useAuth();
-  
+  const { user, openLogin } = useAuth();
 
-    const getInquiries = useCallback(async () => {
-        setError(null);
-        setLoading(true);
-        try {
-            const data = await getUserInquiries();
-            
-            setInquiries(data);
-        } catch (error) {
-            setError(
-                error.message || "Something went wrong while fetching inquiries"
-            );
-        } finally {
-            setLoading(false);
-        }
-    }, []);
+  const getInquiries = useCallback(async () => {
+    setError(null);
+    setLoading(true);
+    try {
+      const data = await getUserInquiries();
 
-    const handleAddInquiry = async ({ animalId, message }) => {
-        setLoading(true);
-    
-        try {
-          await addInquiry({animalId, message});
-          await getInquiries();
-        } catch (error) {
-          setError(error.message || "Something went wrong while adding inquiries");
-        } finally {
-          setLoading(false);
-        }
+      setInquiries(data);
+    } catch (error) {
+      setError(
+        error.message || "Something went wrong while fetching inquiries"
+      );
+    } finally {
+      setLoading(false);
     }
-    const requestAddInquiry = async (messageObj) => {
-        if(!user){
-            setPendingMessageObj(messageObj);
-            await openLogin();
-            return;
-        }
-        await handleAddInquiry(messageObj);
+  }, []);
+
+  const handleAddInquiry = async ({ animalId, message }) => {
+    setLoading(true);
+
+    try {
+      await addInquiry({ animalId, message });
+      await getInquiries();
+    } catch (error) {
+      setError(error.message || "Something went wrong while adding inquiries");
+    } finally {
+      setLoading(false);
     }
-
-
-
-    useEffect(() => {
-        if (!user) {
-            setInquiries([]);
-            return;
-        }
-        getInquiries();
-    }, [user, getInquiries]);
-
-    useEffect(() => {
-        if (!user || !pendingMessageObj) return;
-        
-        const addPendingMessage = async ()=>{
-            await handleAddInquiry(pendingMessageObj)
-            setPendingMessageObj(null);
-        }
-        
-        addPendingMessage();
-            
-        
-    }, [pendingMessageObj, user])
-    return (
-      <InquiryContext.Provider
-        value={{
-          inquiries,
-        requestAddInquiry,
-          loading,
-          error,
-        }}
-      >
-        {children}
-      </InquiryContext.Provider>
-    );
   };
-  
-  export const useInquiry = () => useContext(InquiryContext);
+  const requestAddInquiry = async (messageObj) => {
+    if (!user) {
+      setPendingMessageObj(messageObj);
+      await openLogin();
+      return;
+    }
+    await handleAddInquiry(messageObj);
+  };
+
+  useEffect(() => {
+    if (!user) {
+      setInquiries([]);
+      return;
+    }
+    getInquiries();
+  }, [user, getInquiries]);
+
+  useEffect(() => {
+    if (!user || !pendingMessageObj) return;
+
+    const addPendingMessage = async () => {
+      await handleAddInquiry(pendingMessageObj);
+      setPendingMessageObj(null);
+    };
+
+    addPendingMessage();
+  }, [pendingMessageObj, user]);
+  return (
+    <InquiryContext.Provider
+      value={{
+        inquiries,
+        requestAddInquiry,
+        loading,
+        error,
+      }}
+    >
+      {children}
+    </InquiryContext.Provider>
+  );
+};
+
+export const useInquiry = () => useContext(InquiryContext);
