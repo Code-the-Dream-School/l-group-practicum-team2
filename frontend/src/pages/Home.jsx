e65c17627c40d2aec3f1589d9214bc1dc63453f9
Villy Siu <villysiu@gmail.com>
2026-05-29 07:26:32 -0700

    show skeleton when all animal is loading

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import AnimalList from "../components/animals/AnimalList";
 import SpecialNeedCarousel from "../components/animals/SpecialNeedCarousel";
 import AnimalListPlaceholder from "../components/placeholders/AnimalListPlaceholder";
-import useGlobalLoading from "../components/loading/useGlobalLoading";
-
+// import useGlobalLoading from "../components/loading/useGlobalLoading";
+import { useAnimal } from "../contexts/AnimalContext";
 const Home = () => {
-  const isLoading = useGlobalLoading();
-  if(isLoading)
+  const {loading: animalLoading} = useAnimal();
+  
+  if(animalLoading)
     return <AnimalListPlaceholder />;
+  
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
