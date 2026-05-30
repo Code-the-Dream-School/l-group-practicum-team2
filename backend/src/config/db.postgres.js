baeb8193b18001e335a15555aa40011e89f12f6a
Villy Siu <villysiu@gmail.com>
2026-05-30 14:51:26 -0700

    prevent backend crash by adding pg pool error listener and timeouts

--- backend/src/config/db.postgres.js
+++ backend/src/config/db.postgres.js
@@ -3,6 +3,15 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: true },
+  idleTimeoutMillis: 30000, 
+  max: 10                   
+
+});
+
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle database client:', err.message);
 });
 
 module.exports = pool;
