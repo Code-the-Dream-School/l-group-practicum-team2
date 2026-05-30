999cadf21cf7ae3853e60303c64e753941a515b2
Villy Siu <villysiu@gmail.com>
2026-05-30 14:59:39 -0700

    remove unused lint variable and bypass ssl config during local testing

--- backend/src/config/db.postgres.js
+++ backend/src/config/db.postgres.js
@@ -3,7 +3,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: true },
+  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('neon.tech')
+    ? { rejectUnauthorized: true }
+    : false,
   idleTimeoutMillis: 30000, 
   max: 10                   
 
