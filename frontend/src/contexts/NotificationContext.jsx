c02418546aea11c6fc3b3d782f8ac7212b2cf18e
Villy Siu <villysiu@gmail.com>
2026-05-30 16:45:52 -0700

    add missing dependency to addNotification to fix getFavorites loop

--- frontend/src/contexts/NotificationContext.jsx
+++ frontend/src/contexts/NotificationContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useCallback, useContext, useState } from "react";
 import PropTypes from "prop-types";
 
 const NotificationContext = createContext();
@@ -6,7 +6,13 @@ const NotificationContext = createContext();
 export const NotificationProvider = ({ children }) => {
   const [notifications, setNotifications] = useState([]);
 
-  const addNotification = (type, message) => {
+  const removeNotification = useCallback((id) => {
+    setNotifications((prevNotifications) =>
+      prevNotifications.filter((notification) => notification.id !== id)
+    );
+  }, []);
+
+  const addNotification = useCallback((type, message) => {
     const id = crypto.randomUUID();
 
     const newNotification = {
@@ -23,13 +29,9 @@ export const NotificationProvider = ({ children }) => {
     setTimeout(() => {
       removeNotification(id);
     }, 5000);
-  };
+  }, [removeNotification]);
 
-  const removeNotification = (id) => {
-    setNotifications((prevNotifications) =>
-      prevNotifications.filter((notification) => notification.id !== id)
-    );
-  };
+  
 
   return (
     <NotificationContext.Provider
