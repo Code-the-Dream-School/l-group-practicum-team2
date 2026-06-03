78097a54be84a8d275d5ca69bc619761447ec7a4
Villy Siu <villysiu@gmail.com>
2026-06-02 21:47:55 -0700

    Refactor to use collapseOnSelect with CollapsibleMenu component

--- frontend/src/components/navbar/NavigationBar.jsx
+++ frontend/src/components/navbar/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import { Button, Container, Image, Nav, Navbar } from "react-bootstrap";
-import { HeartFill } from "react-bootstrap-icons";
+import { HeartFill, HouseFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import logo from "../../assets/PawMatch.png";
 import { useAuth } from "../../contexts/AuthContext";
@@ -7,15 +7,16 @@ import UserDropdown from "./UserDropdown";
 import { SigninButton } from "./SigninButton";
 import { useLocation } from "react-router-dom";
 import CollapsibleMenu from "./CollapsibleMenu";
+import { useState } from "react";
 
 const NavigationBar = () => {
-  const { user, logoutUser } = useAuth();
-  const location = useLocation();
+  const { user, logoutUser, openLogin } = useAuth();
+
   return (
-    <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" sticky="top">
       <Container className="px-3 py-4">
-        <Navbar.Brand as={Link} to="/" className="me-3">
-          <Image
+         <Navbar.Brand as={Link} to="/" className="me-3">
+           <Image
             src={logo}
             alt="PawMatch logo"
             className="navbar-logo"
@@ -24,46 +25,47 @@ const NavigationBar = () => {
           <div className="mt-3">Find your perfect companion</div>
         </Navbar.Brand>
 
-        <div className="d-none d-lg-flex gap-3 ms-auto me-2 align-items-center">
-          <Nav className="ms-auto align-items-lg-center gap-3">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-          </Nav>
-          <div>
-            {user ? (
-              <div style={{ display: "flex" }}>
-                <Button
-                  as={Link}
-                  to="/favorites"
-                  variant="danger"
-                  className="px-4 py-2 me-3 fs-5"
-                >
-                  <HeartFill size={25} className="me-3" />
-                  <span className="fs-5">Favorites</span>
-                </Button>
-                <UserDropdown
+          <Nav className="w-100 d-none d-lg-flex flex-row justify-content-between">
+            <div className="fs-4 gap-2">
+              <Nav.Link href="/">
+                <HouseFill size={25} className="me-2"/> Home
+              </Nav.Link>
+            </div>
+            <div className='d-flex flex-row gap-2'>
+
+              {
+                user ?
+                <>
+                  <Button
+                    as={Link}
+                    to="/favorites"
+                    variant="danger"
+                    className="px-4 py-2 fs-5"
+                  >
+                    <HeartFill size={25} className="me-3" />
+                    <span className="fs-5">Favorites</span>
+                  </Button>
+                  <UserDropdown
                   user={user}
                   onLogout={() => logoutUser(true, location.pathname)}
                 />
-              </div>
-            ) : (
-              <SigninButton />
-            )}
-
-        </div>
-      </div>
+                </>
+                :
+                <SigninButton />
+              }
+ 
         <Navbar.Toggle aria-controls="main-navbar-nav" className="d-lg-none" />
   
         <Navbar.Collapse id="main-navbar-nav">
           <div className="d-lg-none">
-            <CollapsibleMenu user={user} onLogout={logoutUser} />
+            <CollapsibleMenu user={user} onLogout={logoutUser}  openLogin={openLogin}/>
           </div>
-
-      
+  
         </Navbar.Collapse>
+              </Nav>
       </Container>
     </Navbar>
+   
   );
 };
 
