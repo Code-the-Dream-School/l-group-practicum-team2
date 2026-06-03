b4e9efcf6a0ba536fe2e898aa8b490d6296fe55f
Villy Siu <villysiu@gmail.com>
2026-06-02 21:43:45 -0700

    Create mobile collapsible menu and center-align links

--- frontend/src/components/navbar/CollapsibleMenu.jsx
+++ frontend/src/components/navbar/CollapsibleMenu.jsx
@@ -1,44 +1,54 @@
 import { Nav } from "react-bootstrap";
 import { HeartFill, PersonFill, HouseFill, EnvelopeHeartFill, BoxArrowRight  } from "react-bootstrap-icons";
-import { useAuth } from "../../contexts/AuthContext";
-const CollapsibleMenu = ({user, onLogout}) => {
-    const { openLogin } = useAuth();
+import { Link } from "react-router-dom";
+const CollapsibleMenu = ({user, onLogout, openLogin}) => {
+
     return (
-        <Nav className="flex-column align-items-center text-center gap-3 py-3 w-100">
-            <Nav.Link to="/">
-                <HouseFill size={25} className="me-1" />
-                Home
-            </Nav.Link>
-            {
-                user ?
-                <>
-                    <Nav.Link  to="/profile">
-                        <PersonFill size={25} className="me-1" />
-                        Profile
-                    </Nav.Link >
-                    <Nav.Link to="/favorites">
-                        <HeartFill size={25} className="me-1" />
-                        <span>Favorites</span>
-                    </Nav.Link >
-                    <Nav.Link  to="/profile/inquiries">
-                        <EnvelopeHeartFill size={25} className="me-1" />
-                        Inquiries
-                    </Nav.Link >
-                    <Nav.Link onClick={onLogout}>
-                        <BoxArrowRight size={25} className="me-1" />
-                        Logout
-                        </Nav.Link>
-                </>
-                : 
-                
-                <Nav.Link onClick={() => openLogin()} >
-                    <PersonFill size={25} className="me-1" />
-                    <span>Sign in</span>
-                    
+        <Nav className="d-lg-none flex-column align-items-center text-center">
+            { user ? 
+            <>
+                <Nav.Link href="/">
+                    <HouseFill size={25} className="me-1" />
+                    Home
                 </Nav.Link>
-                }
+                <Nav.Link href="/profile">
+                    <PersonFill size={25} className="me-1" />
+                    Profile
+                </Nav.Link>
+                <Nav.Link href="/favorites">
+                    <HeartFill size={25} className="me-1" />
+                    Favorites
+                </Nav.Link>
+                
+                <Nav.Link href="/profile/inquiries">
+                    <EnvelopeHeartFill size={25} className="me-1" />
+                    Inquiries
+                </Nav.Link>
+                <Nav.Link 
+                    href="#"
+                    onClick={
+                        (e) => {
+                        e.preventDefault();
+                        onLogout()
+                    }}
+                >
+                    <BoxArrowRight size={25} className="me-1" />
+                    Logout
+                </Nav.Link>
+            </>
+            : 
+            <Nav.Link 
+                href="#"
+                onClick={(e) => {
+                    e.preventDefault();
+                    openLogin();
+                }}
+            >
+                Sign in
+            </Nav.Link>
+            }
             
-        </Nav>
+          </Nav>
     )
 }
 export default CollapsibleMenu
