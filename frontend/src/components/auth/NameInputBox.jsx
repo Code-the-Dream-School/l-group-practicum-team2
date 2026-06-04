eb9c29a63d20e2ab1fd6cf44689ae42298de634a
volha83 <o.podlipskaya@gmail.com>
2026-06-04 04:42:13 -0400

    Fix name validation error clearing

--- frontend/src/components/auth/NameInputBox.jsx
+++ frontend/src/components/auth/NameInputBox.jsx
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 
 const NameInputBox = ({ name, setName, nameError, setNameError }) => {
   const handleChange = (e) => {
-    setName(e.target.value);
-    if (nameError) validateName(e.target.value);
+    const newName = e.target.value;
+    setName(newName);
+
+    if (nameError) {
+      validateName(newName);
+    }
   };
   const validateName = (name) => {
-    const re = /^[a-zA-Z0-9.-_]{2,20}$/;
+    const re = /^[a-zA-Z0-9 ._]{2,20}$/;
 
     if (name === "") setNameError("Name is required");
     else if (!re.test(name))
