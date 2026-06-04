22583a1b2405432b5b57683e1678ea04b1454fb9
Villy Siu <villysiu@gmail.com>
2026-06-04 07:08:22 -0700

    add missing useNavigation hool

--- frontend/src/pages/favorites/FavoritesPage.jsx
+++ frontend/src/pages/favorites/FavoritesPage.jsx
@@ -2,11 +2,12 @@ import { Button, Card } from "react-bootstrap";
 import AnimalCard from "../../components/animals/AnimalCard";
 import AnimalListPlaceholder from "../../components/placeholders/AnimalListPlaceholder";
 import { useFavorite } from "../../contexts/FavoriteContext";
-import { Alert } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function FavoritesPage() {
   const { favoriteAnimals, favoritesLoading } = useFavorite();
-
+  const navigate = useNavigate();
+  
   if (favoritesLoading) {
     return <AnimalListPlaceholder />;
   }
