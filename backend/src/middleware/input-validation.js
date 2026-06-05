1e4b2b43848502ce44b18914e041620e78ba98e6
Villy Siu <villysiu@gmail.com>
2026-06-04 21:06:51 -0700

    run prettier format

--- backend/src/middleware/input-validation.js
+++ backend/src/middleware/input-validation.js
@@ -1,25 +1,24 @@
 const Joi = require('joi');
 const BadRequestError = require('../errors/bad-request');
 const passwordRegex = '^[a-zA-Z0-9]{6,30}$';
-const nameRegex = '^[a-zA-Z0-9 ._]{2,20}$'
+const nameRegex = '^[a-zA-Z0-9 ._]{2,20}$';
 
 const validateRegisterInput = (req, res, next) => {
   const { name, email, password } = req.body;
   const schema = Joi.object({
-    name: Joi.string()
-      .pattern(new RegExp(nameRegex))
-      .required()
-      .messages({
-        "string.pattern.base": "Name must be between 2 and 20 characters and contain only letters, numbers, spaces, dots, or underscores."
-      }),
-    
+    name: Joi.string().pattern(new RegExp(nameRegex)).required().messages({
+      'string.pattern.base':
+        'Name must be between 2 and 20 characters and contain only letters, numbers, spaces, dots, or underscores.',
+    }),
+
     email: Joi.string().email().required(),
     password: Joi.string()
       .pattern(new RegExp(passwordRegex))
       .required()
       .messages({
-        "string.pattern.base": "Password must be between 6 and 30 characters and contain only letters and numbers."
-      })
+        'string.pattern.base':
+          'Password must be between 6 and 30 characters and contain only letters and numbers.',
+      }),
   });
   const valid = schema.validate({ name, email, password });
   if (valid.error) {
@@ -32,9 +31,8 @@ const validateLoginInput = (req, res, next) => {
   const { email, password } = req.body;
   const schema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required()
-   
-    });
+    password: Joi.string().required(),
+  });
   const valid = schema.validate({ email, password });
   if (valid.error) {
     return next(new BadRequestError(valid.error.message));
